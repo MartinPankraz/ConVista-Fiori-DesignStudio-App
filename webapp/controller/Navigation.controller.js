5dfc9e3c503b30d8b9974b90e7f7d34bab7c9b25
Pankraz Martin <martin.pankraz@convista.com>
2016-12-30 17:12:21 +0000

    selection screen draft added

--- webapp/controller/Navigation.controller.js
+++ webapp/controller/Navigation.controller.js
@@ -9,6 +9,7 @@ sap.ui.define([
 
 		onInit: function () {
 			jQuery.sap.require("sap.ui.core.format.DateFormat");
+			jQuery.sap.require("sap.ui.comp.valuehelpdialog.ValueHelpDialog");
 			var sRootPath = jQuery.sap.getModulePath("convista.com.arp.demo");
 			var that = this;
 			this.idPrefix = this.getView().getId()+"--";
@@ -64,6 +65,18 @@ sap.ui.define([
 			/*var userModel = new sap.ui.model.json.JSONModel("/services/userapi/currentUser");
 			var userView = this.getView().byId("username");
 			userView.setModel(userModel, "userapi");*/
+			
+			this.theTokenInput= this.getView().byId("multiInput2");
+			this.theTokenInput.setEnableMultiLineMode( sap.ui.Device.system.phone); 
+	 
+			this.aKeys= ["CompanyCode", "CompanyName"];
+	 
+			var rangeToken1= new sap.m.Token({key: "i1", text: "ID: a..z"}).data("range", { "exclude": false, "operation": "BT", "keyField": "CompanyCode", "value1": "a", "value2": "z"});
+			var rangeToken2= new sap.m.Token({key: "i2", text: "ID: =foo"}).data("range", { "exclude": false, "operation": "EQ", "keyField": "CompanyCode", "value1": "foo", "value2": ""});
+			var rangeToken3= new sap.m.Token({key: "e1", text: "ID: !(=foo)"}).data("range", { "exclude": true, "operation": "EQ", "keyField": "CompanyCode", "value1": "foo", "value2": ""});
+			this.aTokens= [rangeToken1, rangeToken2, rangeToken3];
+			
+			this.theTokenInput.setTokens(this.aTokens);
 		},
 		
 		onNavListItemSelect: function(oEvent){
@@ -222,6 +235,47 @@ sap.ui.define([
 			var iframe = currhtml.getContent();
 			var srcToReload = jQuery(iframe).attr("src");
 			currhtml.setContent("<iframe class='bo_container' src='" + srcToReload + "'></iframe>");
+		},
+		
+		onSelectionScreenRequest: function(oEvent){
+			var that= this;
+		
+			var oValueHelpDialog = new sap.ui.comp.valuehelpdialog.ValueHelpDialog({
+				basicSearchText: this.theTokenInput.getValue(), 
+				title: "Company",
+				supportRanges: true,
+				supportRangesOnly: true, 
+				key: this.aKeys[0],				
+				descriptionKey: this.aKeys[1],
+				stretch: sap.ui.Device.system.phone, 
+	 
+				ok: function(oControlEvent) {
+					that.aTokens = oControlEvent.getParameter("tokens");
+					that.theTokenInput.setTokens(that.aTokens);
+	 
+					oValueHelpDialog.close();
+				},
+	 
+				cancel: function(oControlEvent) {
+					sap.m.MessageToast.show("Cancel pressed!");
+					oValueHelpDialog.close();
+				},
+	 
+				afterClose: function() {
+					oValueHelpDialog.destroy();
+				}
+			});
+			
+			oValueHelpDialog.setRangeKeyFields([{label: "Company Code", key: "CompanyCode"}, {label : "Company Name", key:"CompanyName"}]); 
+			oValueHelpDialog.setTokens(this.theTokenInput.getTokens());
+			
+			if (this.theTokenInput.$().closest(".sapUiSizeCompact").length > 0) { // check if the Token field runs in Compact mode
+				oValueHelpDialog.addStyleClass("sapUiSizeCompact");
+			} else {
+				oValueHelpDialog.addStyleClass("sapUiSizeCozy");			
+			}
+			
+			oValueHelpDialog.open();
 		}
 	});
 
